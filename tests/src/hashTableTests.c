fa718b51e23e7f21d1f17a65da384712eb62efaa
CaioPellicani <caiopellicani@hotmail.com>
2021-05-31 16:29:26 -0300

    IMP: hashTable files

---
+++ tests/src/hashTableTests.c
@@ -0,0 +1,9 @@
+#include "../../src/linked_list.h"
+#include "minunit.h"
+#include "base_tests.h"
+
+runHashTableTest(){
+    INIT_TESTS( "HashTable" );
+
+    END_TESTS;
+}
--- tests/src/main_tests.c
+++ tests/src/main_tests.c
@@ -18,6 +18,7 @@ int main( int argc, char** argv ){
     runOrderDoubleLinkedListTest();
     runBinarySearchTreeTest();
     runTrieTest();
+    runHashTableTest();
     return 0;  
 }
  
